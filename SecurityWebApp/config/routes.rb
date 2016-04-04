a015bc04104a288bd2d2ce6a520e668ad972758b
Sigf <seb.hutt@gmail.com>
2016-04-04 14:12:59 -0700

    Adds item and users models with scaffold controllers and views

--- SecurityWebApp/config/routes.rb
+++ SecurityWebApp/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  resources :items
+  root to: "item#index"
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
