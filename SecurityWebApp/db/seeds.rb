58fd6e07bfe847ee599460dab3cb997ac44e188c
Sigf <seb.hutt@gmail.com>
2016-04-10 23:16:30 -0700

    Removes admin and create seed file

--- SecurityWebApp/db/seeds.rb
+++ SecurityWebApp/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  admins = User.create([
+    {email: 'admin@app.com', password: "a", password_confirmation: "a", is_admin:1}
+    ])
+
+  users = User.create([
+    {email: 'a@a.com', password: "a", password_confirmation: "a", is_admin:0}
+    ])
+
+  items = Item.create([
+    {name: "AA", description: "", image: "", price: 10, stock: 1},
+    {name: "AB", description: "", image: "", price: 120, stock: 4},
+    {name: "AC", description: "", image: "", price: 40, stock: 2},
+    {name: "CD", description: "", image: "", price: 30, stock: 12},
+    {name: "WD", description: "", image: "", price: 20, stock: 15}
+    ])
